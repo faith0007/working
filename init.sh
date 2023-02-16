964007224a226eafbcb268e08e01a8be23ad7f41
faith0007 <119907154+faith0007@users.noreply.github.com>
2023-02-16 09:03:08 +0900

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install net-tools
+sudo apt install iputils-ping
+pip install httpie
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
